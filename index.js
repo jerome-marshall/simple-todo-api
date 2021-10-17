d5798b6813321a82ca35f8ca4bef00baf2252209
Jerome Marshall <jeromemarshall0@gmail.com>
2021-10-17 11:12:35 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const cors = require("cors")
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`LIstening on port ${port}`))
